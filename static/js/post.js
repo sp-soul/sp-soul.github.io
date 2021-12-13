f50173563d82ccf1d6f15ef7abb20de6e520cf8d
MrFirthy <ashley@mrfirthy.me>
2021-12-13 17:49:25 +0000

    Cleanup JS and SVG markup

--- static/js/post.js
+++ static/js/post.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let progressElem = document.querySelector('.progress__circle');
-    let progressPath = document.querySelector('.progress path');
-    let pathLength = progressPath.getTotalLength();
+    let progressElem = document.querySelector('.progress__circle'),
+        progressPath = document.querySelector('.progress path'),
+        pathLength = progressPath.getTotalLength();
     progressPath.style.transition = progressPath.style.WebkitTransition =
     'none';
     progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
@@ -11,9 +11,9 @@ document.addEventListener("DOMContentLoaded", () => {
     'stroke-dashoffset 300ms linear, fill 300ms linear';
 
     let updateProgress = function () {
-        let scroll = window.pageYOffset | document.body.scrollTop;
-        let height = document.body.clientHeight - window.innerHeight;
-        let progress = pathLength - (scroll * pathLength / height);
+        let scroll = window.pageYOffset | document.body.scrollTop,
+            height = document.body.clientHeight - window.innerHeight,
+            progress = pathLength - (scroll * pathLength / height);
         progressPath.style.strokeDashoffset = progress;
         console.log(progress);
         if(progress == 0) {
